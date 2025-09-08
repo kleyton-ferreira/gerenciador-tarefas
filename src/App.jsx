8424a4085f605f93ece303d49fbedd1c1e2d55bd
Kleyton Ferreira <kleytondev40@gmail.com>
2025-09-08 17:56:49 -0300

    chore: add tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React from 'react'
 
 function App() {
   return (
     <div>
-      <h1> Wellcome FSC! </h1>
+      <h1 className="mt-5 bg-brand-dark-blue p-5 text-brand-primary">
+        Wellcome FSC!
+      </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
