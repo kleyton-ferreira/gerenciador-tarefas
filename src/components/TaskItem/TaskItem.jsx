6accdcdfa51a420e1d0ecb56c7c6e7b6e0ffbce1
Kleyton Ferreira <kleytondev40@gmail.com>
2025-09-10 16:36:59 -0300

    feat: ensure TaskItem changes depending on task status

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { CheckedIcon, DetailsIcon, LoaderIcon } from '../../assets/icons'
+
 const TaskItem = ({ taskItens }) => {
   const getStatusClass = () => {
     if (taskItens.status === 'done') {
@@ -10,13 +12,35 @@ const TaskItem = ({ taskItens }) => {
     }
 
     if (taskItens.status === 'not-started') {
-      return 'bg-brand-text-gray  text-brand-text-gray'
+      return 'bg-brand-text-gray bg-opacity-15 text-brand-text-gray'
     }
   }
 
   return (
-    <div className={`rounded-[10px] bg-opacity-10 p-3 ${getStatusClass()}`}>
-      <p>{taskItens.title}</p>
+    <div
+      className={`flex items-center justify-between rounded-[10px] bg-opacity-10 p-3 ${getStatusClass()}`}
+    >
+      <div className="flex items-center gap-3">
+        <label
+          className={`relative z-10 flex h-7 w-7 cursor-pointer rounded-lg px-4 py-3 text-sm ${getStatusClass()})}`}
+        >
+          <input
+            type="checkbox"
+            checked={taskItens.status === 'done'}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {taskItens.status === 'done' && (
+            <CheckedIcon className="absolute right-2 top-2" />
+          )}
+          {taskItens.status === 'in-progress' && (
+            <LoaderIcon className="absolute right-2 top-2 animate-spin" />
+          )}
+        </label>
+        <p>{taskItens?.title}</p>
+      </div>
+      <a href="#">
+        <DetailsIcon className="text-brand-text-gray transition-all hover:opacity-80" />
+      </a>
     </div>
   )
 }
