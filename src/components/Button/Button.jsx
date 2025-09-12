a15bddbfef186384c1d8de4338b60bbbe0832b84
Kleyton Ferreira <kleytondev40@gmail.com>
2025-09-12 18:10:43 -0300

    feat: add size and size and tertiary to button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import { tv } from 'tailwind-variants'
 
-const Button = ({ children, variant, ...rest }) => {
+const Button = ({ children, variant, className, size, ...rest }) => {
   const button = tv({
     base: 'flex items-center gap-2 rounded-md px-3 py-1 text-[16px] transition hover:bg-opacity-70',
     variants: {
       color: {
         primary: 'bg-brand-primary text-brand-white',
         secondary: 'bg-transparent text-brand-dark-gray hover:text-opacity-70',
+        tertiary:
+          'bg-brand-light-gray text-brand-dark-blue hover:text-opacity-70',
         ghost: 'cursor-pointer text-brand-text-gray hover:text-opacity-70',
       },
+      size: {
+        small: 'w-full px-3 py-2 text-[14px]',
+        larger: 'w-full px-3 py-2 text-[14px]',
+      },
+    },
+    defaultVariants: {
+      color: 'primary',
     },
   })
 
   return (
     <>
-      <button className={button({ color: variant })} {...rest}>
+      <button className={button({ color: variant, className, size })} {...rest}>
         {children}
       </button>
     </>
