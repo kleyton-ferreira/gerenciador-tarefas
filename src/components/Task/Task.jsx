3e61d060f2806c153f0b43db911a31b20c424839
Kleyton Ferreira <kleytondev40@gmail.com>
2025-09-30 20:41:20 -0300

    feat: ensure empty message is shown on Task component

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -98,6 +98,11 @@ const Task = () => {
       <div className="mt-6 rounded-[10px] bg-brand-white p-6">
         <div className="space-y-3">
           <TaskSeparator title="Manhã" icon={<SunIcon />} />
+          {morningTasks.length === 0 && (
+            <p className="text- text-brand-text-gray">
+              Nenhuma tarefa cadastrada para o período da manhã.
+            </p>
+          )}
           {morningTasks.map((task) => (
             <TaskItem
               key={task.id}
@@ -109,6 +114,12 @@ const Task = () => {
         </div>
         <div className="my-8 space-y-3">
           <TaskSeparator title="Tarde" icon={<CloudIcon />} />
+          {afterTasks.length === 0 && (
+            <p className="text- text-brand-text-gray">
+              Nenhuma tarefa cadastrada para o período da tarde.
+            </p>
+          )}
+
           {afterTasks.map((task) => (
             <TaskItem
               key={task.id}
@@ -120,6 +131,11 @@ const Task = () => {
         </div>
         <div className="space-y-3">
           <TaskSeparator title="Noite" icon={<MoonIcon />} />
+          {eveningTasks.length === 0 && (
+            <p className="text- text-brand-text-gray">
+              Nenhuma tarefa cadastrada para o período da noite.
+            </p>
+          )}
           {eveningTasks.map((task) => (
             <TaskItem
               key={task.id}
